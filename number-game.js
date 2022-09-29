03c2692bc9148ea1a6cf7797c3de6939d6694a23
benjibro425 <112976306+benjibro425@users.noreply.github.com>
2022-09-29 09:33:24 -0400

    redo2

--- number-game.js
+++ number-game.js
@@ -6,5 +6,5 @@ module.exports = {
 		.setDescription('Guess what number I am thinking'),
 	async execute(interaction) {
 		await interaction.reply('I am thinking of a number between 1 and 100, what is it?');
-	}
+	},
 };
